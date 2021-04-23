56672ce1c01e3e6e95bec2af94cf1b0c97d0d9eb
rayfazt <rayfazt@gmail.com>
2021-04-24 05:25:59 +0700

    skill

--- src/main/java/Element.java
+++ src/main/java/Element.java
@@ -1,5 +1,71 @@
-public interface Element {
+public class Element {
+    private ElementType type;
 
-    public ElementType getType();
-    public double getAdvantage(ElementType enemyType);
+    public Element(ElementType type) {
+        this.type = type;
+    }
+
+    public ElementType getType() {
+        return this.type;
+    }
+
+    public double getAdvantage(ElementType enemyType) {
+        switch (enemyType) {
+            case FIRE:
+                if (this.type == ElementType.WATER) {
+                    return 2;
+                } else if (this.type == ElementType.GROUND) {
+                    return 1.5;
+                } else if (this.type == ElementType.ICE) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+
+            case ICE:
+                if (this.type == ElementType.FIRE) {
+                    return 2;
+                } else if (this.type == ElementType.ELECTRIC) {
+                    return 1.5;
+                } else if (this.type == ElementType.GROUND) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+
+            case WATER:
+                if (this.type == ElementType.ELECTRIC) {
+                    return 2;
+                } else if (this.type == ElementType.FIRE) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+
+            case ELECTRIC:
+                if (this.type == ElementType.GROUND) {
+                    return 2;
+                } else if (this.type == ElementType.ICE) {
+                    return 0.5;
+                } else if (this.type == ElementType.WATER) {
+                    return 0;
+                } else {
+                    return 0;
+                }
+
+            case GROUND:
+                if (this.type == ElementType.ICE) {
+                    return 2;
+                } else if (this.type == ElementType.FIRE) {
+                    return 0.5;
+                } else if (this.type == ElementType.ELECTRIC) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+
+            default:
+                return 0;
+        }
+    }
 }
---
+++ src/main/java/Skill.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class Skill {
+    private String skillName;
+    private int basePower;
+    private int masteryLevel;
+    private ArrayList<ElementType> learnableElements;
+
+    public Skill() {
+        this.skillName = "defaultSkill";
+        this.basePower = 0;
+        this.masteryLevel = 1;
+    }
+
+    public Skill(String name, int bp, int ml, ElementType type) {
+        this.skillName = name;
+        this.basePower = bp;
+        this.masteryLevel = ml;
+        this.learnableElements.add(type);
+    }
+
+    public String getSkillName() {
+        return this.skillName;
+    }
+
+    public int getBasePower() {
+        return this.basePower;
+    }
+
+    public int getMasteryLevel() {
+        return this.masteryLevel;
+    }
+
+    public ArrayList<ElementType> getLearnableElements() {
+        return this.learnableElements;
+    }
+
+    public void setMasteryLevel(int level) {
+        this.masteryLevel = level;
+    }
+
+}
