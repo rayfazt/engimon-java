3912a7ace227f7f8e96f86babac68ee4501fb009
aisyahfarrasaqila <aisyahfarrasaqila@gmail.com>
2021-04-23 13:05:35 +0700

    add ui kasar

---
+++ src/sample/Main.java
@@ -0,0 +1,98 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.animation.Timeline;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.util.Duration;
+import javafx.scene.image.Image;
+
+import java.awt.*;
+
+public class Main extends Application {
+
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = WIDTH;
+    private static final int ROWS = 20;
+    private static final int COLUMNS = ROWS;
+    private static final int SQUARE_SIZE = WIDTH / ROWS;
+
+    private static final String pikachu = "/img/pikachu.png";
+    private Image engimonImage;
+    private final int engimonX = 7;
+    private final int engimonY = 12;
+
+    private GraphicsContext gc;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Engimon");
+        Group root = new Group();
+        Canvas canvas = new Canvas(WIDTH,HEIGHT);
+        root.getChildren().add(canvas);
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        gc = canvas.getGraphicsContext2D();
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(130), e -> run(gc)));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+    }
+
+    private void run(GraphicsContext gc) {
+        drawBackground(gc);
+        drawEngimon(gc);
+    }
+
+    private void drawBackground(GraphicsContext gc) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if ((i + j) % 2 == 0) {
+                    gc.setFill(Color.web("AAD751"));
+                } else {
+                    gc.setFill(Color.web("A2D149"));
+                }
+                gc.fillRect(i * SQUARE_SIZE, j * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+            }
+        }
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 10; j < COLUMNS; j++) {
+                if ((i + j) % 2 == 0) {
+                    gc.setFill(Color.web("AAD751"));
+                } else {
+                    gc.setFill(Color.web("A2D149"));
+                }
+                gc.fillRect(i * SQUARE_SIZE, j * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+            }
+        }
+        for (int i = 10; i < ROWS; i++) {
+            for (int j = 0; j < 10; j++) {
+                if ((i + j) % 2 == 0) {
+                    gc.setFill(Color.web("B3ECFF"));
+                } else {
+                    gc.setFill(Color.web("99E6FF"));
+                }
+                gc.fillRect(i * SQUARE_SIZE, j * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+            }
+        }
+    }
+
+    private void drawEngimon(GraphicsContext gc) {
+        engimonImage = new Image(pikachu);
+        gc.drawImage(engimonImage, engimonX * SQUARE_SIZE, engimonY * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
