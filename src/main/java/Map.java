2dbe486d33a4f776aaeb717d3479b09f9782bf68
aisyahfarrasaqila <aisyahfarrasaqila@gmail.com>
2021-04-24 21:35:50 +0700

    map & mapreader udh bisa

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -2,4 +2,55 @@ import java.util.*;
 
 public class Map {
     private ArrayList<Point> map; //Masih bingung ke game.java nya gmn
+
+    public Map(String filename, String separator){
+        this.map = new ArrayList<Point>();
+        MapReader mapRead = new MapReader(filename, separator);
+        try {
+            List<String[]> lines = mapRead.read();
+            int i = 0;
+            for (String[] line : lines) {
+                int j = 0;
+                for (String point : line) {
+                    int x = i;
+                    int y = j;
+                    System.out.println(point);
+                    if (point.equals("m")) {
+                        Point coord = new Point(x,y,MapType.MOUNTAINS);
+                        this.map.add(coord);
+                    }
+                    else if (point.equals("s")) {
+                        Point coord = new Point(x,y,MapType.SEA);
+                        this.map.add(coord);
+                    }
+                    else if (point.equals("g")) {
+                        Point coord = new Point(x,y,MapType.GRASSLAND);
+                        this.map.add(coord);
+                    }
+                    else {
+                        // TUNDRA
+                        Point coord = new Point(x,y,MapType.TUNDRA);
+                        this.map.add(coord); 
+                    }
+                    j++;
+                }
+                i++;
+            }
+        } catch (Exception e) {
+            System.out.println("File not found");
+        }
+    }
+
+    public void printMapPoints() {
+        for (Point point : map) {
+            point.printPoint();
+        }
+    }
+
+    /*
+    public static void main(String[] args) {
+        Map map = new Map("test.txt", "");
+        map.printMapPoints();
+    }
+    */
 }
---
+++ src/main/java/MapReader.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapReader {
+    private File txtFile;
+    private String separator;
+    
+    public MapReader(String filename, String separator){
+        this.txtFile = new File(filename);
+        this.separator = separator;
+    }
+
+    public List<String[]> read() throws IOException {
+        String line;
+        FileReader fileReader = new FileReader(this.txtFile);
+        BufferedReader br = new BufferedReader(fileReader);
+        ArrayList<String[]> list = new ArrayList<String[]>();
+
+        while ((line = br.readLine()) != null) {
+            String[] row = line.split(this.separator);
+            for (int i = 0; i < row.length; i++) {
+                String val = row[i];
+                if (val.length() >= 2 && val.charAt(0) == '"' && val.charAt(val.length() - 1) == '"') {
+                    row[i] = val.substring(1, val.length() - 1);
+                }
+            }
+            list.add(row);
+        }
+        return list;
+    }
+}
