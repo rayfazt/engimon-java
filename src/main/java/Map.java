b7216602476143f5d73a8b6598722cf9b0e6198c
aisyahfarrasaqila <aisyahfarrasaqila@gmail.com>
2021-04-25 11:24:42 +0700

    add arraylist buat masing2 lingkungan (buat random move)

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -1,13 +1,22 @@
 import java.util.*;
 
 public class Map {
-    private ArrayList<Point> map; //Masih bingung ke game.java nya gmn
+    private ArrayList<Point> map; // All points
+    private ArrayList<Point> mountains;
+    private ArrayList<Point> sea;
+    private ArrayList<Point> grassland;
+    private ArrayList<Point> tundra;
+
     private int row;
     private int col;
 
     public Map(String filename, String separator){
         try {
             this.map = new ArrayList<Point>();
+            this.mountains = new ArrayList<Point>();
+            this.sea = new ArrayList<Point>();
+            this.grassland = new ArrayList<Point>();
+            this.tundra = new ArrayList<Point>();
             int i,j;
             MapReader mapRead = new MapReader(filename, separator);
             List<String[]> lines = mapRead.read();
@@ -22,19 +31,23 @@ public class Map {
                     if (point.equals("m")) {
                         Point coord = new Point(x,y,MapType.MOUNTAINS);
                         this.map.add(coord);
+                        this.mountains.add(coord);
                     }
                     else if (point.equals("s")) {
                         Point coord = new Point(x,y,MapType.SEA);
                         this.map.add(coord);
+                        this.sea.add(coord);
                     }
                     else if (point.equals("g")) {
                         Point coord = new Point(x,y,MapType.GRASSLAND);
                         this.map.add(coord);
+                        this.grassland.add(coord);
                     }
                     else {
                         // TUNDRA
                         Point coord = new Point(x,y,MapType.TUNDRA);
-                        this.map.add(coord); 
+                        this.map.add(coord);
+                        this.tundra.add(coord);
                     }
                     j++;
                 }
@@ -65,11 +78,24 @@ public class Map {
         for (Point point : map) {
             point.printPoint();
         }
+        System.out.println("\n");
+        for (Point point : mountains) {
+            point.printPoint();
+        }
+        for (Point point : sea) {
+            point.printPoint();
+        }
+        for (Point point : grassland) {
+            point.printPoint();
+        }
+        for (Point point : tundra) {
+            point.printPoint();
+        }
     }
 
     /*
     public static void main(String[] args) {
-        Map map = new Map("test.txt", "");
+        Map map = new Map("src/main/resources/test.txt", "");
         map.printMapPoints();
     }
     */
