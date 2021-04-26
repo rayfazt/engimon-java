ed44c3a2e8e309c47b5992d11e6214ae1e70b7d7
rayfazt <rayfazt@gmail.com>
2021-04-26 22:06:13 +0700

    fix import

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,22 +1,30 @@
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
 import javafx.scene.control.Tooltip;
-import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.animation.Timeline;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
 import javafx.util.Duration;
+import javafx.scene.image.Image;
+import javafx.scene.control.Button;
+
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.security.Key;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 
 public class Game extends Application {
 
